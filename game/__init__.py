199f52b79a75a8d227ad82261876c9dd02e4ad99
312-05\helena1005 <helena1005@naver.com>
2024-12-26 10:06:58 +0900

    파이썬 패키지 연습(클래스 모듈 __init__)

---
+++ game/__init__.py
@@ -0,0 +1,14 @@
+from .graphic.render import render_test
+from .sound.echo import echo_test
+
+VERSION = "3.11.2"
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}.")
+    print("게임 제작자: 이혜린")
+    print("게임 제작자 메일: helena1005@naver.com")
+    print("게임 제작 의뢰: 010-6449-6852")
+
+#패키지 실행 초기화 문구
+print("Initializing game······")
+print("Loading···")
---
+++ game/graphic/render.py
@@ -0,0 +1,5 @@
+from game.sound.echo import echo_test
+
+def render_test():
+    print("render_test 메서드 실행···")
+    echo_test()
